9384e1cb9097752422d06b22a9a149289759317c
unknown <navyas211@gmail.com>
2021-11-14 19:13:31 +0530

    [NAVYA]ADDED: update the salary i.e. the base pay for Employee Terisa to 3000000.00 and sync it with Database.

---
+++ src/main/java/com/BL/EmpServiceJdbc/EmployeeTableUpdate.java
@@ -0,0 +1,61 @@
+/*
+ * UC3:- Ability to update the salary i.e. the base pay for Employee Terisa to 3000000.00 and sync it with Database . 
+ * 
+ * @author : Navaya Shree
+ * @since : 13-11-2021
+ */
+package com.BL.EmpServiceJdbc;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class EmployeeTableUpdate {    
+	static Statement stmt;
+	static Connection con;
+	public static void main(String[] args) throws Exception {
+
+		Connection con = null;
+		Statement stmt = null;
+		String qry = "update employee_payroll.payroll_service set basic_pay=3000000.00 where name='Terisa'";
+		try {
+			System.out.println("Driver Class Loaded");
+
+			con =ConnectionDB.createCP();
+
+			System.out.println("Connetion Establish with db server");
+
+			stmt = con.createStatement();
+			System.out.println("Platform Created");
+
+			stmt.executeUpdate(qry);
+			System.out.println("Data Update");
+
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+
+		finally {
+			if (stmt != null) {
+				try {
+					stmt.close();
+					System.out.println("Closed All Resources");
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+
+			}
+			if (con != null) {
+				try {
+					con.close();
+					System.out.println("Closed All Resources");
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+
+			}
+		}
+
+	}
+	
+}
