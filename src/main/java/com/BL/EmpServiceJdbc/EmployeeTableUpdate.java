d89be2d31d2ad6ada34ad32f75f6619997c7fbbc
unknown <navyas211@gmail.com>
2021-11-14 20:00:33 +0530

    [NAVYA]Refactor: To retrieve all employees who have joined in a particular data range from the payroll service database.

--- src/main/java/com/BL/EmpServiceJdbc/EmployeeTableUpdate.java
+++ src/main/java/com/BL/EmpServiceJdbc/EmployeeTableUpdate.java
@@ -1,5 +1,5 @@
 /*
- * UC3:- Ability to update the salary i.e. the base pay for Employee Terisa to 3000000.00 and sync it with Database . 
+ * UC5:- Ability to retrieve all employees who have joined in a particular data range from the payroll service database .
  * 
  * @author : Navaya Shree
  * @since : 13-11-2021
@@ -7,37 +7,67 @@
 package com.BL.EmpServiceJdbc;
 
 import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class EmployeeTableUpdate {    
-	static Statement stmt;
+	static PreparedStatement pstmt;
 	static Connection con;
+	static ResultSet rs;
 	public static void main(String[] args) throws Exception {
-
-		String qry = "update employee_payroll.payroll_service set basic_pay=3000000.00 where name='Terisa'";
+		String qry = "select * from employee_payroll.payroll_service where start between Cast('2020-01-01' as date) and date (now())";
 		try {
+			Class.forName("com.mysql.jdbc.Driver");
+
 			System.out.println("Driver Class Loaded");
 
-			con =ConnectionDB.createCP();
+			con = ConnectionDB.createCP();
 
 			System.out.println("Connetion Establish with db server");
 
-			stmt = con.createStatement();
-			System.out.println("Platform Created");
-
-			stmt.executeUpdate(qry);
+			pstmt = con.prepareStatement(qry);
 			System.out.println("Data Update");
+
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				int id = rs.getInt(1);
+				String name = rs.getString(2);
+				long phone_number = rs.getLong(3);
+				String address = rs.getString(4);
+                String department = rs.getString(5);
+				String gender = rs.getString(6);
+				double basic_pay = rs.getDouble(7);
+				double deductions = rs.getDouble(8);
+				double taxable_pay = rs.getDouble(9);
+				double tax = rs.getDouble(10);
+				double met_pay = rs.getDouble(11);
+				Date state = rs.getDate(12);
+
+				System.out.println("EmpId :-" + id + "  " +"EmpName :- " + name + " " + "Emp_PhoneNo. :-" + phone_number + " " + "EmpAddress :- " + address + " " + " EmpDepartment :-" + department + " "
+						+ "Gender :- "+ gender + " " + "Basic_Pay " +  basic_pay + " " + " Deduction :- " + deductions + " " + "Taxable_pay :- " + taxable_pay + " " + "Tax :-" +tax + " " + "Met_Pay :- " + met_pay
+						+ " " + "Date :-" +  state );
+			}
 
 		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
 		}
 
 		finally {
-			if (stmt != null) {
+			if (rs != null) {
 				try {
-					stmt.close();
-					System.out.println("Closed All Resources");
+					rs.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+
+			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+
 				} catch (SQLException e) {
 					e.printStackTrace();
 				}
@@ -46,14 +76,13 @@ public class EmployeeTableUpdate {
 			if (con != null) {
 				try {
 					con.close();
-					System.out.println("Closed All Resources");
 				} catch (SQLException e) {
 					e.printStackTrace();
 				}
 
 			}
+			System.out.println("Closed All Resources");
 		}
 
 	}
-	
 }
